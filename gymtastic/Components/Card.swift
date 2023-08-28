4ec6b23999bc80728452330a20c1e4f58226fa82
Tom Kwok <work@tomkwok.xyz>
2023-08-28 19:39:26 +1000

    Sync GitHub with local Xcode

---
+++ gymtastic/Components/Card.swift
@@ -0,0 +1,20 @@
+//
+//  Card.swift
+//  gymtastic
+//
+//  Created by Tom Kwok on 8/27/23.
+//
+
+import SwiftUI
+
+struct Card: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Card_Previews: PreviewProvider {
+    static var previews: some View {
+        Card()
+    }
+}
