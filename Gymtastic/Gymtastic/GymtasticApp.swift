11cef536375c1fbbf7ef83d87269503b70209603
Tom Kwok <work@tomkwok.xyz>
2023-09-09 15:20:46 +1000

    New project

---
+++ Gymtastic/Gymtastic/GymtasticApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymtasticApp.swift
+//  Gymtastic
+//
+//  Created by Tom Kwok on 9/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GymtasticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
